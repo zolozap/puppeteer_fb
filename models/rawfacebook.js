5ce73a93b7665e597a08015a8cd00f3cc7248fb0
Natapong Sripeung <natapong.sripeung@gmail.com>
2021-06-16 16:09:45 +0700

    Debug

--- models/rawfacebook.js
+++ models/rawfacebook.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const db = mongoose.connect(process.env.MONGOCLIENT_CONNECT, { 
+var db = mongoose.connect(process.env.MONGOCLIENT_CONNECT, { 
     dbName: 'aion',
     useNewUrlParser: true, 
     useUnifiedTopology: true
