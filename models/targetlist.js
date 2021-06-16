a5296d68fe5e1d41d784298aff5d7ac38eaab7c6
Natapong Sripeung <natapong.sripeung@gmail.com>
2021-06-16 15:37:40 +0700

    Debug

--- models/targetlist.js
+++ models/targetlist.js
@@ -4,24 +4,24 @@ const Schema = mongoose.Schema
 
 const aion = mongoose.createConnection(process.env.MONGOCLIENT_CONNECT.concat('target_list'));
 
-// const target_listSchema = new Schema({
-//     _id: mongoose.ObjectId,
-//     link_crawl: String,
-//     crawled: Boolean,
-//     insert_timestamp: Date,
-//     last_time: Date,
-//     latest: Date,
-//     link_original: String,
-//     source: String,
-//     target_type: String,
-//     type: String,
-//     uid: String,
-//     status: Number,
-//     crawling:Boolean,
-//     email: String,
-//     telnumber: String
-// })
+const target_listSchema = new Schema({
+    _id: mongoose.ObjectId,
+    link_crawl: String,
+    crawled: Boolean,
+    insert_timestamp: Date,
+    last_time: Date,
+    latest: Date,
+    link_original: String,
+    source: String,
+    target_type: String,
+    type: String,
+    uid: String,
+    status: Number,
+    crawling:Boolean,
+    email: String,
+    telnumber: String
+})
 
-const TargetListModel = aion.model('target_list')
+const TargetListModel = aion.model('target_list', target_listSchema)
 
 module.exports = TargetListModel
