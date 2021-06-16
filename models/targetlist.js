1fe30cd39ae405d0b3e53ae7d66d055ad7c55f59
Natapong Sripeung <natapong.sripeung@gmail.com>
2021-06-16 15:35:13 +0700

    Debug

--- models/targetlist.js
+++ models/targetlist.js
@@ -2,26 +2,26 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const aion = mongoose.createConnection(process.env.MONGOCLIENT_CONNECT.concat('aion'));
+const aion = mongoose.createConnection(process.env.MONGOCLIENT_CONNECT.concat('target_list'));
 
-const target_listSchema = new Schema({
-    _id: mongoose.ObjectId,
-    link_crawl: String,
-    crawled: Boolean,
-    insert_timestamp: Date,
-    last_time: Date,
-    latest: Date,
-    link_original: String,
-    source: String,
-    target_type: String,
-    type: String,
-    uid: String,
-    status: Number,
-    crawling:Boolean,
-    email: String,
-    telnumber: String
-})
+// const target_listSchema = new Schema({
+//     _id: mongoose.ObjectId,
+//     link_crawl: String,
+//     crawled: Boolean,
+//     insert_timestamp: Date,
+//     last_time: Date,
+//     latest: Date,
+//     link_original: String,
+//     source: String,
+//     target_type: String,
+//     type: String,
+//     uid: String,
+//     status: Number,
+//     crawling:Boolean,
+//     email: String,
+//     telnumber: String
+// })
 
-const TargetListModel = aion.model('target_list',target_listSchema)
+const TargetListModel = aion.model('target_list')
 
 module.exports = TargetListModel
